af8d346d4eaa9cb8e1eecebb712db512c044dfc5
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2017-12-13 17:59:28 -0600

    Deploy token, sale and funding sale.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,37 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
+const FRNUToken = artifacts.require('FRNUToken');
+const FRNUTokenCrowdsale = artifacts.require('FRNUTokenCrowdsale');
 
 module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
+  const acc1 = process.env.add1 || web3.eth.accounts[0];
+  const acc2 = process.env.add2 || web3.eth.accounts[1];
+
+  deployer.then(() => {
+    let token, sale;
+    return deployer.deploy(FRNUToken,
+      Date.now(),
+      '1546300800',
+      acc2,
+      acc2,
+      acc2,
+      acc2
+    );
+  }).then(() => FRNUToken.deployed()).then((instance) => {
+    token = instance;
+
+
+    return deployer.deploy(FRNUTokenCrowdsale,
+      1,
+      Date.parse('Feb 28, 2018'),
+      170,
+      acc1,
+      token.address
+    );
+  }).then(() => FRNUTokenCrowdsale.deployed()).then((instance) => {
+    sale = instance;
+
+    return token.balanceOf.call(acc1);
+  }).then((val) => {
+    token.transfer(sale.address, val)
+  })
+
 };
