95857b41a2cc14bd9bc96742593017b7ebdfc9a3
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2017-12-14 15:53:33 -0600

    Updating with some differnt possible configs.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,12 +9,14 @@ module.exports = function(deployer) {
     web3.personal.unlockAccount(acc1, pw);
   }
 
+  // deployer.deploy(FRNCoin)
+
   deployer.deploy(FRNCoinCrowdsale,
     1,
     Date.parse('Feb 28, 2018'),
     170,
     acc1,
-    '0x77ae4cded8c197b4c503895368f077ef6288462b',
-    '0x64c6f8c1d931978522e36008074a17f89b9016c9'
+    '0xaccaf49428bce5e5787c6ff86c49f32769af8360',
+    acc1
   );
 };
