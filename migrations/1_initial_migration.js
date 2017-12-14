6697f3ecbcd9b9041507631285cb0c160dfd9744
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2017-12-14 13:55:25 -0600

    Adding conditional unlock

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
+
+  const acc1 = process.env.add1 || web3.eth.accounts[0];
+  const pw  = process.env.add1Password;
+
+  if(pw) {
+    web3.personal.unlockAccount(acc1, pw);
+  }
+
   deployer.deploy(Migrations, { overwrite: false });
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,11 @@ const FRNUTokenCrowdsale = artifacts.require('FRNUTokenCrowdsale');
 
 module.exports = function(deployer) {
   const acc1 = process.env.add1 || web3.eth.accounts[0];
-  const acc2 = process.env.add2 || web3.eth.accounts[1];
+  const pw  = process.env.add1Password;
 
+  if(pw) {
+    web3.personal.unlockAccount(acc1, pw);
+  }
   deployer.then(() => {
     let token, sale;
     return deployer.deploy(FRNUToken,
