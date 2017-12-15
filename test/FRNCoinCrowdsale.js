5f1a2a43f0af4b3f043857f92cb4f9ca0039cabf
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2017-12-15 13:07:48 -0600

    Adding a mostly working test.  Need to fix sorce of the send.

--- test/FRNCoinCrowdsale.js
+++ test/FRNCoinCrowdsale.js
@@ -3,11 +3,30 @@ const FRNCoinCrowdsale = artifacts.require("./FRNCoinCrowdsale.sol");
 
 contract('FRNCoinCrowdsale', async function(accounts) {
 
-  it("awards tokens from ether", async () => {
-    // sale = await FRNCoinCrowdsale.deployed();TODO: deploy new in test
-    // token = await FRNCoin.deployed();
+  coinbase = accounts[0];
+  tokenReceiver = accounts[1];
+  tokenPool = accounts[2];
+  wallet = accounts[3];
 
-    // assert.equal(true, await sale.buyTokens.call('0x11123f0013', { value: 1 }));
-    // assert.equal(await token.balanceOf('0x11123f0013').toString(), await sale.rate.call().toString())
+  it("awards tokens from ether", async () => {
+    token = await FRNCoin.new();
+    sale = await FRNCoinCrowdsale.new(
+      Date.parse('Feb 28, 2018'),
+      170,
+      wallet,
+      token.address,
+      tokenPool
+    );
+
+    await token.transfer(tokenPool, await token.balanceOf(coinbase));
+    await token.approve(sale.address, await token.balanceOf(tokenPool), { from: tokenPool });
+
+    web3.eth.coinbase = tokenReceiver
+    console.log(await sale.send(1));
+
+
+
+
+    assert.equal((await token.balanceOf(tokenReceiver)).toString(), 170);
   });
 });
